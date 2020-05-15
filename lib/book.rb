838c3b03ed905c5d202b229339777f7f2c09a340
Bob Whitmer <rjwhitmercc@gmail.com>
2020-05-14 20:52:08 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    def initialize title, author = nil, page_count = 0, genre = nil
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
 
+    attr_accessor :title, :author, :page_count, :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
 
